b8c12b564993245900614462cb5ccc5f0fca64a5
azmialwerfalli <drama.com.aa@gmail.com>
2023-02-07 22:33:54 +0200

    modified:   src/App.vue 	modified:   src/router/index.js 	new file:   src/views/jobs/JobDetailsView.vue 	new file:   src/views/jobs/JobsView.vue

---
+++ src/views/jobs/JobsView.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <h2>{{ job.title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return  {
+            jobs: [
+                {title: "a full stack Master", id:1,details:"Master Programmer that has no life"}
+        ,        {title: "a front end Develoer", id:2,details:"just a cute child that put boxes above each other"}
+         ,       {title: "a back end developer", id:3,details:"that other child that's loud and always mad"}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
