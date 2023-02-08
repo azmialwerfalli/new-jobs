7d0a4c228fbf127aca4ee5aec1e9b966daffdef0
azmialwerfalli <drama.com.aa@gmail.com>
2023-02-08 20:48:24 +0200

    modified:   src/views/jobs/JobsView.vue

--- src/views/jobs/JobsView.vue
+++ src/views/jobs/JobsView.vue
@@ -10,9 +10,9 @@ export default {
     data() {
         return  {
             jobs: [
-                {title: "a full stack Master", id:1,details:"Master Programmer that has no life"}
-        ,        {title: "a front end Develoer", id:2,details:"just a cute child that put boxes above each other"}
-         ,       {title: "a back end developer", id:3,details:"that other child that's loud and always mad"}
+                {title: "a full stack Master", id:1,details:"Master Programmer that has no life"},        
+                {title: "a front end Develoer", id:2,details:"just a cute child that put boxes above each other"},
+                {title: "a back end developer", id:3,details:"that other child that's loud and always mad"}
             ]
         }
     }
